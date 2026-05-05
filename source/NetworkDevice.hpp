73317811b8365cb79b88c6b50e45cd8f4b49f459
L Carver <wifihum@gmail.com>
2026-05-04 18:36:25 -0700

    Works with print() member functions.

---
+++ source/NetworkDevice.hpp
@@ -0,0 +1,132 @@
+//------------------------------------------------------------------------------
+// Base class NetworkDevice declaration and definition
+// Derived class Router declaration and definition
+// Derived class Switch declaration and definition
+//------------------------------------------------------------------------------
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+//------------------------------------------------------------------------------
+// base class
+//------------------------------------------------------------------------------
+class NetworkDevice
+{
+public:
+    static unsigned assetCount;
+    unsigned assetID;
+    unsigned ports;
+    std::string deviceType;
+    std::string name;
+    std::string location;
+    std::string connectedTo;
+
+    // constructor
+    NetworkDevice() : ports(0)
+    {
+        assetID = ++assetCount;
+    }
+
+    // destructor
+    virtual ~NetworkDevice() { }
+
+    // derived classes must provide
+    virtual void setDeviceSpecific(std::string data) = 0;
+
+    // derived classes will override
+    virtual void print() const
+    {
+        std::cout << deviceType << " " << name
+            << " is in " << location << "\n";
+        std::cout << "Asset ID: " << assetID << "\n";
+        std::cout << "Ports: " << ports << "\n";
+        std::cout << "Connected to: " << connectedTo << "\n";
+    }
+
+    // overload + operator
+    unsigned operator+(NetworkDevice& rhs)
+    {
+        return rhs.ports;
+    }
+};
+
+//------------------------------------------------------------------------------
+// derived classes
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Router
+//------------------------------------------------------------------------------
+class Router : public NetworkDevice
+{
+public:
+    std::string protocols;
+
+    // constructor
+    Router()
+    {
+        deviceType = "Router";
+    }
+
+    // derived classes must provide
+    void setDeviceSpecific(std::string data)
+    {
+        protocols = data;
+    }
+
+    // overrides base class
+    void print() const
+    {
+        NetworkDevice::print();
+        std::cout << "Routing protocols: " << protocols << "\n";
+    }
+};
+
+//------------------------------------------------------------------------------
+// Switch
+//------------------------------------------------------------------------------
+class Switch : public NetworkDevice
+{
+public:
+    std::string vlans;
+
+    // constructor
+    Switch()
+    {
+        deviceType = "Switch";
+    }
+
+    // derived classes must provide
+    void setDeviceSpecific(std::string data)
+    {
+        vlans = data;
+    }
+
+    // overrides base class
+    void print() const
+    {
+        NetworkDevice::print();
+        std::cout << "Configured with VLANS: " << vlans << "\n";
+    }
+};
+
+//------------------------------------------------------------------------------
+// wrapper class for deallocating memory
+//------------------------------------------------------------------------------
+class VectorWrapper
+{
+public:
+    std::vector<NetworkDevice*> vpDevices;
+
+    // constructor
+    VectorWrapper() = default;
+
+    // destructor
+    ~VectorWrapper()
+    {
+        for (NetworkDevice* pND : vpDevices)
+            delete pND;
+    }
+};
--- source/PA12_SQEEK_Network_Devices.cpp
+++ source/PA12_SQEEK_Network_Devices.cpp
@@ -1,6 +1,5 @@
-//------------------------------------------------------------------------------
-// PA12_SQEEK_Network_Devices.cpp
-//------------------------------------------------------------------------------
+#include "NetworkDevice.hpp"
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -15,22 +14,37 @@ const std::string INPUT_FILENAME = "PA12_network_devices.txt";
 //------------------------------------------------------------------------------
 // local function prototypes
 //------------------------------------------------------------------------------
-void readFile();
+void readFile(std::vector<NetworkDevice*>& vpDevices);
+static void setDeviceData(NetworkDevice*pND, std::stringstream& ss);
+
+//------------------------------------------------------------------------------
+// NetworkDevice static member
+//------------------------------------------------------------------------------
+unsigned NetworkDevice::assetCount = 0;
 
 //------------------------------------------------------------------------------
 // entry point
 //------------------------------------------------------------------------------
 int main()
 {
-    readFile();
-    std::cout << "Hello World!\n";
-}
+    VectorWrapper vDevices;
 
+    readFile(vDevices.vpDevices);
+
+    std::cout << "Number of network device assets :"
+        << NetworkDevice::assetCount << "\n\n";
+
+    for (NetworkDevice* pDevice : vDevices.vpDevices)
+    {
+        pDevice->print();
+        std::cout << "\n";
+    }
+}
 
 //------------------------------------------------------------------------------
 // read input file and display
 //------------------------------------------------------------------------------
-void readFile()
+void readFile(std::vector<NetworkDevice*>& vpDevices)
 {
     std::ifstream input(INPUT_FILENAME);
     std::string line;
@@ -39,20 +53,44 @@ void readFile()
 
     while (getline(input, line))
     {
-        std::vector<std::string> result;
         std::stringstream ss(line);
 
-        while (std::getline(ss, token, ','))
-        {
-            result.push_back(token);
-        }
+        // use base class pointer
+        NetworkDevice* pND = nullptr;
 
-        for (const auto& word : result)
+        std::getline(ss, token, ',');
+        if (!token.compare("Router"))
         {
-            std::cout << word << "\n";
+            pND = new Router();
         }
+        else
+        {
+            pND = new Switch();
+        }
+        setDeviceData(pND, ss);
+        std::getline(ss, token, ',');
+        pND->setDeviceSpecific(token);
+
+        vpDevices.push_back(pND);
     }
-
     // done with file
     input.close();
 }
+
+//------------------------------------------------------------------------------
+// build Router or Switch instance from stringstream data
+//------------------------------------------------------------------------------
+static void setDeviceData(NetworkDevice*pND, std::stringstream& ss)
+{
+    std::string token;
+
+    std::getline(ss, token, ',');
+    pND->name = token;
+    std::getline(ss, token, ',');
+    pND->ports = std::stoi(token);
+    std::getline(ss, token, ',');
+    pND->location = token;
+    std::getline(ss, token, ',');
+    pND->connectedTo = token;
+}
+
